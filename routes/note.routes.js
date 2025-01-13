8b73fcabc9590f8c4fca2e471b246e271c933a1f
Sarthak sharma <sarthak312005@gmail.com>
2025-01-13 08:36:35 +0530

    Added Key Check

--- routes/note.routes.js
+++ routes/note.routes.js
@@ -1,7 +1,9 @@
 import express from "express";
 import controller from "../controller/note.controller.js";
+import checkKeys from "../middleware/checkKeys.js";
 
 const router = express.Router();
+router.use(checkKeys);
 
 router.post("/notes", controller.createNote);
 router.get("/notes", controller.getAllNotes);
