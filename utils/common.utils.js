16dbc9b079410b8c22a4b02715e69a28d3b6b90e
Garima Sharma <sharmagarima814@gmail.com>
2025-01-24 09:40:01 +0530

    fix: merge conflicts

--- utils/common.utils.js
+++ utils/common.utils.js
@@ -322,7 +322,6 @@ const generateAlphanumeric = () => {
   return result;
 };
 
-<<<<<<< HEAD
 const referralCode = async (uid) => {
   return new Promise(async (resolve, reject) => {
     try {
@@ -375,9 +374,6 @@ const referralCode = async (uid) => {
 
 export {
   referralCode,
-=======
-export {
->>>>>>> 886df06 (complete: ref code functionality)
   generateAlphanumeric,
   get_id_origin,
   httpRequest,
