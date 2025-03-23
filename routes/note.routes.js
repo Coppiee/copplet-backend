bc4bb093239e0621fad26a323207b2282c9edb1f
Sarthak sharma <sarthak312005@gmail.com>
2025-03-23 15:59:41 +0530

    add: notes routes

---
+++ routes/note.routes.js
@@ -0,0 +1,12 @@
+import express from "express";
+import controller from "../controller/note.controller.js";
+import checkKeys from "../middleware/checkKeys.js";
+
+const router = express.Router();
+
+router.post("/notes", checkKeys, controller.createNote);
+router.get("/notes", checkKeys, controller.getAllNotes);
+router.put("/notes/:noteId", checkKeys, controller.updateNote);
+router.delete("/notes/:noteId", checkKeys, controller.deleteNote);
+
+export default router;
